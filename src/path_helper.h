f1bd90f7975d912096e1b74c4d8b492b55471f67
Max Mustermann <max.mustermann7643@web.de>
2015-03-09 19:34:37 +0100

    Added license

--- src/path_helper.h
+++ src/path_helper.h
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2015 NIPE-SYSTEMS
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 char *concatenate_paths(char *prefix, char *suffix);
 int compare_paths(char *path0, long long int path0_length, char *path1, long long int path1_length);
 int path_exclude_pattern_add(char *pattern);
