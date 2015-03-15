4906c43ab463c8dec99d13f8de99b9f3438327cf
Max Mustermann <max.mustermann7643@web.de>
2015-03-15 13:10:39 +0100

    Completed new index concept

--- backup.sh
+++ backup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 bin/backup \ #
-	--archive "/home/hendrik/backup/initial_backup.tar.gz" \ # the archive to store
+	--archive "/home/user/backup/initial_backup.tar.gz" \ # the archive to store
 	--compress gzip \ # compression algorithm: GZIP
-	--index "/home/hendrik/backup/index" \ # existing index directory (should be empty on first use)
-	--s "/home/hendrik/Programme/can" \ # backup directory which should be 'backupped'
-	--e "/boot/*" \ # exclude shell wildcard pattern for all files in /boot
-	--e "/dev/*" \ # exclude shell wildcard pattern for all files in /dev
-	--e "/mnt/*" \ # exclude shell wildcard pattern for all files in /mnt
-	--e "/proc/*" \ # exclude shell wildcard pattern for all files in /proc
-	--e "/run/*" \ # exclude shell wildcard pattern for all files in /run
-	--e "/sys/*" \ # exclude shell wildcard pattern for all files in /sys
-	--e "/tmp/*" \ # exclude shell wildcard pattern for all files in /tmp
-	--e "/home/hendrik/backup/*" \ # exclude shell wildcard pattern for all files in /
-	--v # verbose output
-	--f # full initial backup
+	--index "/home/user/backup/index" \ # existing index directory (should be empty on first use)
+	--source "/home/user/Programme/can" \ # backup directory which should be 'backupped'
+	--exclude "/boot/*" \ # exclude shell wildcard pattern for all files in /boot
+	--exclude "/dev/*" \ # exclude shell wildcard pattern for all files in /dev
+	--exclude "/mnt/*" \ # exclude shell wildcard pattern for all files in /mnt
+	--exclude "/proc/*" \ # exclude shell wildcard pattern for all files in /proc
+	--exclude "/run/*" \ # exclude shell wildcard pattern for all files in /run
+	--exclude "/sys/*" \ # exclude shell wildcard pattern for all files in /sys
+	--exclude "/tmp/*" \ # exclude shell wildcard pattern for all files in /tmp
+	--exclude "/home/user/backup/*" \ # exclude shell wildcard pattern for all files in the backup directory (don't backup the backup ;) )
+	--verbose \ # verbose output
+	--full # full initial backup
